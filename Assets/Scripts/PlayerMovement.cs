cef68f8435d301f00bf65914cd13fc755996421c
nlapole <nick.lapole@gmail.com>
2023-01-14 21:00:14 -0600

    Tilemap and Player Sprite
    tutorial contents made via Udemy tutorial, "Learn Unity in C#..."
    
    I hope this serves as an initial playground! A playground with week to week goals, of course :)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    // 3 Minutes into "Jumpy Jump" tutorial, part of the Udemy "Learn Unity in C#..."
+    void OnJump(InputValue value)
+    {
+        if(value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+}
