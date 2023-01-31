9b5905c0b043adf86f5208c49bd538074529fae7
nlapole <nick.lapole@gmail.com>
2023-01-31 11:06:45 -0600

    Cleaned WaveAI Method; added more ground
    Debating:
    - Where to place script that takes players from a collision (player - Wave) back to the main menu (in the stead of a "Lose" screen)?

--- Assets/Scripts/WaveAI.cs
+++ Assets/Scripts/WaveAI.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WaveAI : MonoBehaviour
 {
     public GameObject player;
     public float speed;
     public float distanceBetween;
-
     private float distance;
 
     void Start()
@@ -15,6 +15,11 @@ public class WaveAI : MonoBehaviour
 
     }
     void Update()
+    {
+        WaveChasing();
+    }
+
+    void WaveChasing()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
@@ -26,6 +31,5 @@ public class WaveAI : MonoBehaviour
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(Vector3.forward);
         }
-
     }
 }
