b4872d7e1a355325da24fef0e484938e999d8e10
carlbisenius12 <carlbisenius@gmail.com>
2023-02-18 16:15:07 -0600

    Pause Menu as Overlay within Scene with Continue Working
    Continue works, but will still need to adjust Main Menu to work from Pause menu like Quit. Not sure what's going on there. Also will need to disable controls during pause because you can still kind of control Skinker and it disables the Menu from being toggled so you have to use the mouse. Will need to add Controller support too

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+       
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Fire3"))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+}
